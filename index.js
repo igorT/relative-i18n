0520a5662b6fb936c334734ee36cb95fa68ff7b1
IgorT <terzicigor@gmail.com>
2014-10-13 18:17:43 +0300

    init

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+var replace = require('broccoli-replace');
+
+var options =  {
+  files: [
+   '**/*.hbs' // replace only html files in src
+ ],
+ patterns: [
+   {
+     match: 'igor',
+     replacement: 'seb'
+   }
+ ]
+};
+
 module.exports = {
-  name: 'relative-i18n'
+ name: 'relative-i18n',
+ included: function(app) {
+   this._super.included.apply(this, arguments);
+
+     app.registry.add('templates', {
+       name: 'relative-i18n',
+       ext: 'templates',
+       toTree: function(tree) {
+         return replace(tree, options);
+       }
+     });
+   }
+
 };
